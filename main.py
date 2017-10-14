9b1ee9307fc5906053102aeac6f3eb72c2cd36be
echo <lab@uga>
2017-10-14 03:54:17 -0400

    added login page and get contact list

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import dryscrape # NOTE: This package is not actively maintained. It uses QtWebkit, which is end-of-life and probably doesn't get security fixes backported. Consider using a similar package like Spynner instead.
+'''
+@author: 'echo'
+'''
 
-def getPage():
+import dryscrape # NOTE: This package is not actively maintained. It uses QtWebkit, which is end-of-life and probably doesn't get security fixes backported. Consider using a similar package like Spynner instead.
+import webkit_server
+
+import qrcode_terminal
+
+DEBUG = True
+def getLoginPage(URL):
+	# //img[@mm-src-load="qrcodeLoad"] for the login image
 	try:
 		session = dryscrape.Session()
 	except webkit_server.NoX11Error: 
@@ -16,14 +25,23 @@ def getPage():
 	session.set_timeout(10)
 	session.set_header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36")
 	try:
-		session.visit(startUrl)
+		session.visit(URL)
 	except Exception as e:
 		if DEBUG:
 			print(type(e), e.args)
+	for link in session.xpath('//img[@mm-src-load="qrcodeLoad"]'):
+		qrcode_terminal.draw(link["src"].replace("qrcode", "l"))
 
+	session.wait_for(lambda: session.at_xpath('//div[@ng-repeat="chatContact in chatList track by chatContact.UserName"]//span[@class="nickname_text ng-binding"]'))
+	return session
+
+def getRecentContactList(session):
+	for link in session.xpath('//div[@ng-repeat="chatContact in chatList track by chatContact.UserName"]//span[@class="nickname_text ng-binding"]'):
+		print(link.text())
 
 def main():
-	pass
-	
+	session = getLoginPage("https://wx.qq.com/")
+	getRecentContactList(session)
+
 if __name__ == "__main__":
 	main()
